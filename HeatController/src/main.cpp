c97c8af13d0641725cc8f59bbc9c4898c0cb8a43
n3ro <christoph.bubeck@chbu.org>
2024-11-08 16:10:47 +0100

    init

--- HeatController/src/main.cpp
+++ HeatController/src/main.cpp
@@ -4,13 +4,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include <ESP8266mDNS.h>
 
-// WLAN Einstellungen
-const char* ssid = "TempController";
-const char* password = "12345678";
-
-// Webserver auf Port 80
-ESP8266WebServer server(80);
+#define DEBUG_SERIAL
 
 // Pins
 #define ONE_WIRE_BUS 2
@@ -18,7 +14,6 @@ ESP8266WebServer server(80);
 #define MOSFET2_PIN 5
 
 // Temperatur-Einstellungen
-float TARGET_TEMP = 22.0;    // Jetzt als Variable, nicht mehr als Define
 #define TEMP_TOLERANCE 0.5
 #define MAX_PWM 1023
 #define MIN_PWM 0
@@ -28,15 +23,26 @@ float TARGET_TEMP = 22.0;    // Jetzt als Variable, nicht mehr als Define
 #define Ki 0.1
 #define CONTROL_INTERVAL 1000
 
-// OneWire Setup
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-DeviceAddress sensor1, sensor2;
+// WLAN Einstellungen
+const char* ssid = "TempController";
+const char* password = "12345678";
 
-// Regelungsvariablen
+// DNS Server Einstellungen
+const byte DNS_PORT = 53;
+
+// Globale Variablen
+float TARGET_TEMP = 22.0;
+float avgTemp = 0;
 unsigned long lastControlTime = 0;
 float integral = 0;
 int currentPWM = 0;
+
+// Objekte initialisieren
+ESP8266WebServer server(80);
+DNSServer dnsServer;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensor1, sensor2;
 
 // HTML Seite
 const char index_html[] PROGMEM = R"rawliteral(
@@ -64,118 +70,165 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-float avgTemp = 0;  // Global für Webzugriff
+// Funktionsdeklarationen
+static void handleRoot();
+static void handleSetTemp();
+static void handleNotFound();
+static float calculateHeatingPower(float currentTemp);
 
-DNSServer dnsServer;
-const byte DNS_PORT = 53;
-const int CAPTIVE_PORTAL_TIMEOUT = 5000;  // 5 Sekunden Timeout
-
-void handleRoot() {
-  String html = String(index_html);
-  html.replace("%TEMP%", String(avgTemp, 1));
-  html.replace("%TARGET%", String(TARGET_TEMP, 1));
-  html.replace("%PWM%", String((float)currentPWM/MAX_PWM * 100, 0));
-  server.send(200, "text/html", html);
+// Funktionsimplementierungen
+static void handleRoot() {
+    #ifdef DEBUG_SERIAL
+        Serial.println("Hauptseite aufgerufen");
+        Serial.print("Client IP: ");
+        Serial.println(server.client().remoteIP());
+    #endif
+    
+    String html = String(index_html);
+    html.replace("%TEMP%", String(avgTemp, 1));
+    html.replace("%TARGET%", String(TARGET_TEMP, 1));
+    html.replace("%PWM%", String((float)currentPWM/MAX_PWM * 100, 0));
+    server.send(200, "text/html", html);
 }
 
-void handleSetTemp() {
-  if (server.hasArg("temp")) {
-    float newTemp = server.arg("temp").toFloat();
-    if (newTemp >= 5 && newTemp <= 30) {  // Sicherheitsbereich
-      TARGET_TEMP = newTemp;
+static void handleSetTemp() {
+    if (server.hasArg("temp")) {
+        float newTemp = server.arg("temp").toFloat();
+        if (newTemp >= 5 && newTemp <= 30) {
+            TARGET_TEMP = newTemp;
+        }
     }
-  }
-  server.sendHeader("Location", "/");
-  server.send(302, "text/plain", "Updated");
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "Updated");
 }
 
-void handleNotFound() {
-  server.sendHeader("Location", String("http://") + WiFi.softAPIP().toString(), true);
-  server.send(302, "text/plain", "");
+static void handleNotFound() {
+    #ifdef DEBUG_SERIAL
+        Serial.print("Nicht gefundene URL: ");
+        Serial.println(server.uri());
+        Serial.print("Host Header: ");
+        Serial.println(server.hostHeader());
+        Serial.print("Client IP: ");
+        Serial.println(server.client().remoteIP());
+    #endif
+    
+    String redirectUrl = String("http://") + WiFi.softAPIP().toString();
+    server.sendHeader("Location", redirectUrl, true);
+    server.send(302, "text/plain", "");
+}
+
+static float calculateHeatingPower(float currentTemp) {
+    float error = TARGET_TEMP - currentTemp;
+    
+    if (abs(error) < 2.0) {
+        integral += error * (CONTROL_INTERVAL / 1000.0);
+    }
+    
+    integral = constrain(integral, -10, 10);
+    float output = (Kp * error) + (Ki * integral);
+    output = constrain(output, MIN_PWM, MAX_PWM);
+    
+    return output;
 }
 
 void setup() {
-  Serial.begin(9600);
-  
-  // WiFi AP Mode Setup
-  WiFi.softAP(ssid, password);
-  
-  // DNS Server starten für Captive Portal
-  IPAddress apIP = WiFi.softAPIP();
-  dnsServer.start(DNS_PORT, "*", apIP);
-  
-  Serial.print("AP IP Adresse: ");
-  Serial.println(apIP);
-
-  // Webserver Routen
-  server.on("/", handleRoot);
-  server.on("/settemp", handleSetTemp);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  
-  pinMode(MOSFET1_PIN, OUTPUT);
-  pinMode(MOSFET2_PIN, OUTPUT);
-  
-  analogWriteFreq(1000);
-  analogWriteRange(MAX_PWM);
-  
-  sensors.begin();
-  
-  if(sensors.getDeviceCount() < 2) {
-    Serial.println("FEHLER: Nicht genügend Temperatursensoren gefunden!");
-    while(1) { delay(1000); }
-  }
-  
-  sensors.getAddress(sensor1, 0);
-  sensors.getAddress(sensor2, 1);
-  sensors.setResolution(sensor1, 12);
-  sensors.setResolution(sensor2, 12);
-}
-
-float calculateHeatingPower(float currentTemp) {
-  float error = TARGET_TEMP - currentTemp;
-  
-  if (abs(error) < 2.0) {
-    integral += error * (CONTROL_INTERVAL / 1000.0);
-  }
-  
-  integral = constrain(integral, -10, 10);
-  float output = (Kp * error) + (Ki * integral);
-  output = constrain(output, MIN_PWM, MAX_PWM);
-  
-  return output;
+    Serial.begin(9600);
+    
+    Serial.println("\nStarte WiFi Access Point...");
+    
+    WiFi.mode(WIFI_AP);
+    WiFi.persistent(false);
+    
+    Serial.println("Konfiguriere AP...");
+    bool cfgResult = WiFi.softAPConfig(IPAddress(8,8,8,8), IPAddress(8,8,8,8), IPAddress(255,255,255,0));
+    Serial.println(cfgResult ? "AP Konfiguration OK" : "AP Konfiguration fehlgeschlagen");
+    
+    bool apResult = WiFi.softAP(ssid, password);
+    Serial.println(apResult ? "AP Start OK" : "AP Start fehlgeschlagen");
+    
+    Serial.print("AP IP: ");
+    Serial.println(WiFi.softAPIP());
+    
+    dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+    dnsServer.start(DNS_PORT, "*", IPAddress(8,8,8,8));
+    Serial.println("DNS Server gestartet");
+    
+    server.on("/", handleRoot);
+    server.on("/settemp", handleSetTemp);
+    server.on("/generate_204", handleRoot);
+    server.on("/fwlink", handleRoot);
+    server.onNotFound(handleNotFound);
+    server.begin();
+    
+    pinMode(MOSFET1_PIN, OUTPUT);
+    pinMode(MOSFET2_PIN, OUTPUT);
+    
+    analogWriteFreq(1000);
+    analogWriteRange(MAX_PWM);
+    
+    sensors.begin();
+    
+    if(sensors.getDeviceCount() < 2) {
+        Serial.println("FEHLER: Nicht genügend Temperatursensoren gefunden!");
+        while(1) { delay(1000); }
+    }
+    
+    sensors.getAddress(sensor1, 0);
+    sensors.getAddress(sensor2, 1);
+    sensors.setResolution(sensor1, 12);
+    sensors.setResolution(sensor2, 12);
 }
 
 void loop() {
-  dnsServer.processNextRequest();
-  
-  server.handleClient();  // Webserver Anfragen bearbeiten
-  
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - lastControlTime >= CONTROL_INTERVAL) {
-    lastControlTime = currentMillis;
+    static unsigned long lastClientCheck = 0;
+    static uint8_t lastClientCount = 0;
     
-    sensors.requestTemperatures();
-    float temp1 = sensors.getTempC(sensor1);
-    float temp2 = sensors.getTempC(sensor2);
+    dnsServer.processNextRequest();
+    server.handleClient();
     
-    avgTemp = (temp1 + temp2) / 2.0;
-    currentPWM = (int)calculateHeatingPower(avgTemp);
+    if (millis() - lastClientCheck > 5000) {
+        lastClientCheck = millis();
+        uint8_t currentClientCount = WiFi.softAPgetStationNum();
+        
+        if (currentClientCount != lastClientCount) {
+            Serial.print("Verbundene Clients: ");
+            Serial.println(currentClientCount);
+            lastClientCount = currentClientCount;
+        }
+        
+        #ifdef DEBUG_SERIAL
+            Serial.print("Freier Heap: ");
+            Serial.println(ESP.getFreeHeap());
+        #endif
+    }
     
-    analogWrite(MOSFET1_PIN, currentPWM);
-    analogWrite(MOSFET2_PIN, currentPWM);
-    
-    Serial.print("Ziel: ");
-    Serial.print(TARGET_TEMP);
-    Serial.print("°C, Aktuell: ");
-    Serial.print(avgTemp);
-    Serial.print("°C (S1: ");
-    Serial.print(temp1);
-    Serial.print("°C, S2: ");
-    Serial.print(temp2);
-    Serial.print("°C), PWM: ");
-    Serial.print((float)currentPWM/MAX_PWM * 100);
-    Serial.println("%");
-  }
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastControlTime >= CONTROL_INTERVAL) {
+        lastControlTime = currentMillis;
+        
+        sensors.requestTemperatures();
+        float temp1 = sensors.getTempC(sensor1);
+        float temp2 = sensors.getTempC(sensor2);
+        
+        avgTemp = (temp1 + temp2) / 2.0;
+        currentPWM = (int)calculateHeatingPower(avgTemp);
+        
+        analogWrite(MOSFET1_PIN, currentPWM);
+        analogWrite(MOSFET2_PIN, currentPWM);
+        
+        #ifdef DEBUG_SERIAL
+            Serial.println("\n--- Temperatur Update ---");
+            Serial.print("Ziel: ");
+            Serial.print(TARGET_TEMP);
+            Serial.print("°C, Aktuell: ");
+            Serial.print(avgTemp);
+            Serial.print("°C (S1: ");
+            Serial.print(temp1);
+            Serial.print("°C, S2: ");
+            Serial.print(temp2);
+            Serial.print("°C), PWM: ");
+            Serial.print((float)currentPWM/MAX_PWM * 100);
+            Serial.println("%");
+        #endif
+    }
 }
